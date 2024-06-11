a8057cd007a0521f6269da2c8a2b19466ff5cfc2
Nikita8Sannikov <Ak-4t7@mail.ru>
2024-06-11 11:59:10 +0600

    add styles with jsx

--- src/Todo/TodoItem.js
+++ src/Todo/TodoItem.js
@@ -1,10 +1,35 @@
 import React from "react"
 import PropTypes from "prop-types"
 
- function TodoItem({todo, index}){
+const styles = {
+    li: {
+        display:'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '.5rem 1rem',
+        border: '1px solid #ccc',
+        borderRadius: '4px',
+        marginBottom: '.5rem'
+    },
+    input: {
+        marginRight: '1rem'
+    }
+
+}
+
+
+function TodoItem({todo, index}){
     return(
-        <li><strong>{ index+1 }</strong>
-        {todo.title}</li>
+    <li style={styles.li}>
+        <span>
+            <input type="checkbox" style={styles.input}/>
+            
+            <strong>{ index+1 }</strong>
+            {todo.title}
+        </span>
+
+        <button>&times;</button>
+    </li>
     )
 }
 
